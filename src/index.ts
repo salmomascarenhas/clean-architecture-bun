24e66ba90df5a243fd02163a321e3af43013920f
salmomascarenhas <salmo.cruz@gmail.com>
2023-09-23 16:24:06 -0300

    feat: register all cases

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { Elysia } from "elysia";
+import UserRepositoryInMemory from "./external/in-memory/UserRepositoryInMemory";
+import { UserRegister } from "./core/user/service/UserRegister";
+import UserRegisterController from "./adapters/UserRegisterController";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+
+const userRepository = new UserRepositoryInMemory();
+const userRegister = new UserRegister(userRepository);
+new UserRegisterController(app, userRegister);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
